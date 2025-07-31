d353ead0004bf508b814af5494e57e94123548e4
Burgos <santiburgos38@gmail.com>
2025-07-31 17:46:02 -0300

    intentado andar cross-site

--- src/services/servicesLogin.js
+++ src/services/servicesLogin.js
@@ -14,7 +14,7 @@ export const createUser =  async (address_mail)=> {
 export const loggearUser = async (address_mail) =>{
     try{
         const response = await axios.post(`${API}/login`, address_mail,
-            {withCredentials: true})
+            {credentials: 'include'})
         return response.data
     }catch(error){
         console.log('Error al loggear el usuario', error.response?.data || error.message);
